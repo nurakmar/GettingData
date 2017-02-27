e03aaa47c3fc07a3befea8750db88065195bc62d
nurakmar <nurakmar@mohr.gov.my>
2017-02-27 12:04:18 +0800

    script with csv addition
    donwload csv from data.gov.my and open the csv file

--- getting_data.R
+++ getting_data.R
@@ -1,3 +1,10 @@
 library(readr)
 my_data <- read_csv("http://data.stats.gov.my/download_bptms.php?cat=1&id=34")
 View(my_data)
+
+
+
+library(readr)
+download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./labourdata.csv", "auto")
+my_data <- read_csv("./labourdata.csv")
+View(my_data)
